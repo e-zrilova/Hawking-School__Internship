20d5f153b8ff860dd49c99ef2f294aa6b5cb9b8f
Elizaveta <karnizmrkiz@mail.ru>
2021-09-23 13:34:40 +0300

    fix: burger menu

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const swiper = new Swiper('.swiper', {
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   },
 
   // Navigation arrows
@@ -18,21 +19,61 @@ const swiper = new Swiper('.swiper', {
   },
 });
 
-let menuToggle = document.querySelector('#menu-toggle');
-let menu = document.querySelector('.sidebar');
 
-menuToggle.addEventListener('click', function (event) {
-  event.preventDefault();
-  menu.classList.toggle('visible');
-  menuToggle.classList.toggle('is-open');
-})
 
-const header = document.querySelector('.header')
+(function () {
+  const menuToggle = document.querySelector('.js-menu-toggle')
+  const menuItems = document.querySelectorAll('.js-menu-item')
+  const menuContent = document.querySelector('.js-menu-content')
+  const Social = document.querySelector('.js-header__social')
+  const hamburgers = document.querySelector('.hamburger')
 
-window.addEventListener('scroll', () => {
-  if (window.pageYOffset > 0) {
-    header.classList.add('header--dark')
-  } else {
-    header.classList.remove('header--dark')
+  if (menuToggle && Social && menuContent && hamburgers && menuItems.length) {
+    function openMenu() {
+      menuToggle.classList.add('active')
+      menuContent.classList.add('active')
+      Social.classList.add('active')
+      hamburgers.classList.add('active')
+    }
+
+    function closeMenu() {
+      menuToggle.classList.remove('active')
+      menuContent.classList.remove('active')
+      Social.classList.remove('active')
+      hamburgers.classList.remove('active')
+    }
+
+    function toggleMenu() {
+      if (menuToggle.classList.contains('active')) {
+        closeMenu()
+      } else {
+        openMenu()
+      }
+    }
+
+    function resizeHandler() {
+      if (window.innerWidth >= 768) {
+        closeMenu()
+      }
+    }
+    menuToggle.addEventListener('click', toggleMenu)
+    // menuItems.forEach(item => {
+    //   item.addEventListener('click', closeMenu)
+    // })
+    window.addEventListener('resize', resizeHandler)
   }
-})
+}());
+
+(function () {
+const header = document.querySelector('.header')
+const headerHeight = document.querySelector('.first-screen').clientHeight
+
+if (header && headerHeight) {
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > headerHeight) {
+    header.classList.add('js-header--dark')
+  } else {
+    header.classList.remove('js-header--dark')
+  }
+})}
+}());
